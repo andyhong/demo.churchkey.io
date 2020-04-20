15845dc27d3cfc099596c4ad5d571e8085ddc06c
Andy Hong <andyhongoc@gmail.com>
2020-04-20 15:03:38 -0700

    fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,6 +5,8 @@ import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChurch } from '@fortawesome/free-solid-svg-icons'
 
+import "../components/style.css"
+
 const NotFoundPage = () => (
   <div className="wrapper">
     <SEO title="404" />
