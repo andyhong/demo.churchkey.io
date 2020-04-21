403fa7810bb4667fcc038eacb80a0fa92e14b786
Andy Hong <andyhongoc@gmail.com>
2020-04-21 13:12:22 -0700

    added test index page to test netlify identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,10 @@
 import React from "react"
-import { Redirect } from '@reach/router'
 
-const IndexPage = ({data}) => (
+const IndexPage2 = () => (
+  <div className="wrapper">
+    <h1>text index page</h1>
+  </div>
   
-  <Redirect to={`/bulletins/${data.allMarkdownRemark.edges[0].node.frontmatter.date}`} />
 )
 
-export const CurrentBulletinQuery = graphql`
-  query CurrentBulletinQuery {
-    allMarkdownRemark(filter: {isFuture: {eq: false}, frontmatter: {type: {eq: "bulletin"}}}, sort: {fields: frontmatter___date, order: DESC}, limit: 1) {
-      edges {
-        node {
-          frontmatter {
-            date(formatString: "YYYY-MM-DD")
-          }
-        }
-      }
-    }
-  }
-`
-
-export default IndexPage
+export default IndexPage2
