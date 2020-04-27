b733a346e00882dc66c9b23942f74c52d03c0c9e
Andy Hong <andyhongoc@gmail.com>
2020-04-27 12:24:14 -0700

    added hover transitions

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,19 +3,19 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={footerStyle}>
-      <p>Built by <a href="https://instagram.com/andyhxng" name="Andy Hong" target="_blank" rel="noopener noreferrer"><span style={spanStyle}>Andy Hong</span></a></p>
+      <p>Provided by <a href="https://churchkey.netlify.app" target="_blank" rel="noopener noreferrer"><span style={spanStyle}>churchkey</span></a></p>
     </div>
   )
 }
 
 const footerStyle = {
-  margin: '2em 0',
-  fontSize: '1em',
+  color: '#191545',
 }
 
 const spanStyle = {
   color: '#191545',
-  fontWeight: '700',
+  fontWeight: '900',
+  fontSize: '1.2em',
 }
 
 export default Footer
