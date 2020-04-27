c720c58770ac880a71ef130d2e6cc642a031d53f
Andy Hong <andyhongoc@gmail.com>
2020-04-27 14:14:31 -0700

    chore: added css variables for theming

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,20 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={footerStyle}>
-      <p>Provided by <a href="https://churchkey.netlify.app" target="_blank" rel="noopener noreferrer"><span style={spanStyle}>churchkey</span></a></p>
+    <div className="footerWrapper">
+      <p>Provided by <a href="https://churchkey.netlify.app" target="_blank" rel="noopener noreferrer"><span>churchkey</span></a></p>
     </div>
   )
-}
-
-const footerStyle = {
-  color: '#191545',
-}
-
-const spanStyle = {
-  color: '#191545',
-  fontWeight: '900',
-  fontSize: '1.2em',
 }
 
 export default Footer
